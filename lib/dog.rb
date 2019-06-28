f7b4d7c0d78161e2a6cf53c216f6a218324645d7
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-28 17:12:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,8 @@ class Dog
   
   attr_accessor :name
   
-  def inilialize(name, breed)
+  def inilialize(name)
     @name = name 
-    @breed = breed 
-  end
-  
-  def breed 
-    puts "Mutt"
   end
   
 end 
